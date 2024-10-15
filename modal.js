496725376eb20793034815966ef7b52814ad07bc
guilhermelima <guilhermelimaruby@gmail.com>
2024-10-15 15:15:02 -0300

    modal

---
+++ modal.js
@@ -0,0 +1,30 @@
+// modal.js
+// Seleciona elementos
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const captionText = document.getElementById("caption");
+const closeBtn = document.querySelector(".close");
+
+// Adiciona evento para abrir o modal ao clicar na imagem
+const images = document.querySelectorAll(".image-grid img");
+images.forEach(img => {
+    img.addEventListener("click", function () {
+        captionText.textContent = ""
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        // captionText.textContent = this.alt;
+        captionText.innerHTML += `<p>${this.dataset.description}</p>`; // Adiciona a descrição
+    });
+});
+
+// Fecha o modal ao clicar no 'X'
+closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// Fecha o modal ao clicar fora da imagem
+window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
